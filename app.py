896da471313945742514e0ba4e6119fe4775c8b8
Ariane Evans <ariane.evans@student.makeschool.com>
2019-09-29 20:29:23 -0700

    [add] basic setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask (__name__)
+
+@app.route('/')
+def index():
+    """Return homepage."""
+    return render_template ('home.html', msg= 'Flask is cool!')
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
